397f770f27fb79b375b48affe29f575afa83a0e3
deanmalone <deanmalone@users.noreply.github.com>
2017-02-25 23:20:20 +1100

    minor style updates

--- src/app/keyboard/keyboard.component.spec.ts
+++ src/app/keyboard/keyboard.component.spec.ts
@@ -5,17 +5,20 @@ import { DebugElement } from '@angular/core';
 
 import { KeyboardComponent } from './keyboard.component';
 import { PianoService } from '../core/piano.service';
+import { QuizService } from '../core/quiz.service';
 
 describe('KeyboardComponent', () => {
   let component: KeyboardComponent;
   let fixture: ComponentFixture<KeyboardComponent>;
   let pianoService: PianoService = new PianoService();
+  let quizService: QuizService = new QuizService();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ KeyboardComponent ],
       providers:    [
-        {provide: PianoService, useValue: pianoService}
+        {provide: PianoService, useValue: pianoService},
+        {provide: QuizService, useValue: quizService}
       ]
     })
     .compileComponents();
