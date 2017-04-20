e99eb92998edc7820ecfd7c59b2aba83b4b5e56c
Eric M <evm2556@gmail.com>
2017-04-20 19:47:04 -0400

    adding more to e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,39 @@
 import { PianoPlayPage } from './app.po';
+import { browser, element, by } from 'protractor';
 
-describe('Piano Play App', function() {
+describe('Piano Play App', function () {
   let page: PianoPlayPage;
 
   beforeEach(() => {
     page = new PianoPlayPage();
   });
 
-  it('should display App Title', () => {
+  it('check piano keys count', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('PIANO PLAY');
+
+    let piano = element.all(by.css('#piano'));
+    //angular2 protractor does not support repeaters
+    let keyList = element.all(by.css('#piano li'));
+    let keySelect = element.all(by.css('_ngcontent-bxm-1'));
+    keyList.count().then((text) => { console.log("Key Count" + text) });
+    keySelect.count().then((text) => { console.log("Keys Count" + text) });
+    //there should be 29 key
+    expect(keyList.count()).toEqual(29);
+    browser.sleep(1000);
+  });
+  //no expect, hopoing for no error when automating. 
+  it('play song', () => {
+    page.navigateTo();
+    let keyList = element.all(by.css('#piano li'));
+
+    let song = [3, 2, 1, 2, 3, 3, 3, 9, 2, 2, 2, 9, 3, 5, 5, 9, 3, 2, 1, 2, 3, 3, 3, 3, 2, 2, 3, 2, 1]
+    for (let note of song) {
+      browser.sleep(250);
+      if (note == 9)
+        browser.sleep(500);
+      else
+        keyList.get(20 + note).click();
+    }
+    browser.sleep(5000);
   });
 });
